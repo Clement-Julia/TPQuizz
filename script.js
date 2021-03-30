cc02ecaccce14eae1902dead0a0cd09aa647e71e
treflest remy <mrtreflestremy@outlook.com>
2021-03-30 18:03:29 +0200

    voila un mouseover

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const titre = document.querySelector("h1");
 titre.addEventListener('mouseover', function () {
     titre.style.color = "black";
 })
+titre.addEventListener('mouseleave', function () {
+    titre.style.color = "blue";
+})
