5d933913a45eba271a3776a687b2d906fabbbbf7
treflest remy <mrtreflestremy@outlook.com>
2021-04-06 16:32:22 +0200

    mise en forme du projet

--- script.js
+++
@@ -1,7 +0,0 @@
-const titre = document.querySelector("h1");
-titre.addEventListener('mouseover', function () {
-    titre.style.color = "black";
-})
-titre.addEventListener('mouseleave', function () {
-    titre.style.color = "blue";
-})
