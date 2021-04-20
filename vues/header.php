5599251b155f96ea2c738fa65b66c3f9d20610df
treflest remy <mrtreflestremy@outlook.com>
2021-04-20 17:52:48 +0200

    test2

---
+++ vues/header.php
@@ -0,0 +1,75 @@
+<?php
+require_once "../Modele/modele.php";
+$modele = new Modele();
+$url = "/Exo/TPQuizz/vues/index.php";
+$req = $modele->getBdd()->prepare("SELECT libelle from categories");
+$req->execute();
+$Cats = $req->FetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <script src="https://kit.fontawesome.com/f3f16a7b72.js" crossorigin="anonymous"></script>
+    <title>TPQuizz</title>
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <div class="d-flex justify content start">
+          <a class="navbar-brand" href="../vues/">Quiz</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="../vues/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../vues/listeQuizz.php">Liste des Quiz</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="d-flex justify content center position-relative">
+          <?php
+          if($_SERVER['PHP_SELF'] === $url){
+            ?>
+            <button type="button" class="btn dropdown-toggle dropdown-toggle-split radius over" data-bs-toggle="dropdown" aria-expanded="false" style ="width: 150px">
+              Catégories 
+              <span class="visually-hidden"></span>
+            </button>
+            <?php
+          }
+          ?>
+          <ul class="dropdown-menu">
+            <?php
+              foreach($Cats as $Cat){
+                ?>
+                <li><a class="dropdown-item" href="quizz.php?filtre=<?=$Cat["libelle"]?>"><?=$Cat["libelle"]?></a></li>
+                <?php
+              }
+            ?>
+          </ul>
+        </div>
+        <div class="d-flex justify-content-end">  
+          <form class="form-inline mr-3" method="post" action="../traitements/redirection.php">
+            <div class="recherche-barr">
+              <input class="recherche-input" name="filtre" type="search" id="recherche" placeholder="Recherche" aria-label="Search" onclick="addClass()">
+              <button type="submit" class="recherche-icone">
+                <i class="fas fa-search"></i>
+              </button>
+            </div>
+          </form>
+          <?=(!empty($_SESSION["idUtilisateur"]) ? "" : "<a href='inscription.php' class='btn-group btn btn-outline-primary btn-sm align-self-center ml-auto'>Inscription</a>");?>
+          <?=(!empty($_SESSION["idUtilisateur"]) ? "<a href='../traitements/deconnexion.php' class='btn btn-outline-danger btn-sm align-self-center '>Déconnection</a>" : "<a href='connexion.php' class='btn btn-outline-success btn-sm align-self-center'>Connection</a>");?>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
---
+++ vues/index.php
@@ -0,0 +1,69 @@
+<?php
+require_once "header.php";
+require_once "../Modele/modele.php";
+
+$modele = new Modele();
+
+if(empty($_SESSION["idUtilisateur"])){
+    session_destroy();
+}
+
+$req = $modele->getBdd()->prepare("SELECT libelle, icone from categories");
+$req->execute();
+$Cats = $req->FetchAll(PDO::FETCH_ASSOC);
+?>
+
+<div class="d-flex alert alert-secondary mt-3 py-3">
+    Explorez les catégories ou visualisez tous les quizz !
+    <a href="listeQuizz.php" class="ml-auto"><button class="btn btn-primary">
+        Voir tous les quizz <i class="fas fa-chevron-right"></i>
+    </button></a>
+</div>
+
+<div class="mt-5 text-center">
+    <b style ="font-size:25px">Explorez les plus populaires :</b>
+</div>
+<div class="d-flex justify-content-center mt-3">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block carousel" src="https://ahaslides.com/wp-content/uploads/2020/04/cover-1024x578.png" alt="First slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block carousel" src="https://image.freepik.com/vecteurs-libre/theme-fond-neons_52683-44625.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block carousel" src="https://phototrend.fr/wp-content/uploads/2014/12/jpeg-1-759x500.jpg" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+<div class="mt-5 text-center">
+    <b style ="font-size:25px">Explorez par Catégories :</b><br><br>
+    <?php
+    foreach($Cats as $Cat){
+        ?>
+        <div style="width:25%; display:inline-block; margin: 0 20px;" class="mb-4">
+            <a href="quizz.php?filtre=<?=$Cat["libelle"]?>"><button class="btn btn-grey mr-3 py-3 pl-0 pr-3 radius-md policies" style="min-width: 230px;">
+                <img src="<?=$Cat["icone"]?>" style="width:30px; height:30px;" HSPACE="15">
+                <?=$Cat["libelle"]?>
+            </button></a>
+        </div>
+        <?php
+    }
+    ?>
+</div>
+
+
+<?php
+require_once "footer.php";
+?>
